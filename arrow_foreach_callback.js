9e47980562b967de51c4a64f322642e560d37da9
nesterovski <63017385+nesterovski@users.noreply.github.com>
2022-11-18 12:50:55 +0300

    functions, return, arrow practice

---
+++ arrow_foreach_callback.js
@@ -0,0 +1,72 @@
+// arrow functions lesson 33 git 
+// позволяют кратко писать синтаксис функций (чаще всего используется)
+
+
+ // традиционная (полная) функция
+
+//const calcArea = function(radius){
+  //  return 3.14 * radius**2;
+// } закомментил чтобы ниже функция работала
+
+// стрелочная функция
+// слово function убираем, если аргумент в скобках 1, то скобки убираем, если 2 или 0 то оставляем
+
+// const calcArea = radius => {
+//     return 3.14 * radius**2;
+// };
+
+// const area = calcArea(5);
+// console.log('area is:', area);
+
+// можно упрастить еще больше
+// если return на одной строке как выше, то поднимаем его и убираем фигурные и слово return
+
+const calcArea = radius => 3.14 * radius**2;
+
+const area = calcArea(5);
+console.log('area is:', area);
+
+
+// практикуем превращение обычной функции в стрелочную
+
+// const greet = function(){
+// return 'hello, world';
+// }
+
+// console.log(greet());
+
+// ниже упрощаем
+
+const greet = () => 'hello, world';
+console.log(greet());
+
+// еще одно задание по упрощению
+
+// const bill = function(products, tax){
+//   let total = 0;
+//   for(let i = 0; i < products.length; i++){
+//     total += products[i] + products[i] * tax;
+//   }
+//   return total;
+// }
+
+// console.log(bill[10, 15, 30], 0.20);
+
+//ниже упрощаем
+
+const bill = (products, tax) => {
+    let total = 0;
+    for(let i = 0; i < products.length; i++){
+    total += products[i] + products[i] * tax;
+    }
+    return total;
+    };
+
+    console.log(bill[10, 15, 30], 0.20);
+
+
+// for each method & callbacks lesson 35 git
+
+
+
+// callback functions in action
--- functions.js
+++ functions.js
@@ -1,3 +1,73 @@
 // functions
 
-console.log('this file works');
+// function is declared by a function key word followed by a name, followed by parentheses (). 
+
+// function declaration
+
+function greet () {
+    console.log('hello there');
+}
+
+greet(); // называется calling или invoking the function
+
+// function expression 
+// используется когда встраиваем сразу в переменную
+// будет чаще использоваться в жизни чем просто способ function declaration
+
+let speak = function(){
+console.log('good day');
+}; // важно помнить что надо ставить ; если у нас именно function expression, в declaration не надо
+
+speak();
+// function declaration имеет плюс что может быть hoisted 
+// тип если мы в самом конце кода функцию именно так сделаем, то она все равно будет работать
+// в function expression так уже не получится
+
+
+
+// arguments and parameters
+
+// A parentheses can take a parameter. If a function take a parameter it will be called with argument. 
+// A function can also take a default parameter. To store a data to a function, a function has to return certain data types. 
+// To get the value we call or invoke a function
+
+
+let talk = function (name, surname) { // name тут является параметром (как бы создается local переменная, которую можно использовать в функции)
+console.log(`good morning! ${name} ${surname}`);
+};
+
+talk('eugene', 'nesterov'); // если я сюда pass value into the function и она присвоится переменной name в функции
+// можно передавать столько значений, сколько параметров в функции; присваиваться будут по порядку
+
+// можно установить параметры по умолчанию если вдруг туда не будут передаваться значения, пример ниже
+
+let tell = function (name = 'Eugene', surname = 'Nesterov') {
+    console.log(`hey, ${name} ${surname}`);
+};
+
+tell('lu', 'zik'); // если уберу эти значения, то подставяться параметры по умолчанию eugene nesterov
+
+
+
+// return lesson 32 git
+// возвращает результат, который получился после работы функции и этот результат можно записать в переменную чтобы его там хранить
+
+
+function sum(a, b) {
+    return a + b;
+  }
+  
+  let result = sum(1, 2);
+  console.log( result ); // 3
+
+
+const calcArea = function (radius){
+    let area = 3.14 * radius**2;
+    return area;
+}
+
+console.log(calcArea(5));
+
+
+
+

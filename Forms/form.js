101a71568c621cdf390e0139ddf21a63d732dee3
Eugene <mydeepsix@ya.ru>
2022-12-08 14:40:47 +0300

    trivial edits to forms

--- Forms/form.js
+++ Forms/form.js
@@ -1,1 +1,8 @@
-console.log('hey');
+
+// достаем форму и именно на нее вешаем event listener
+// если вешать именно на кнопку то нажимая при вводе инфы
+// на enter то эти данные не дойдут
+
+let username = document.querySelector('form');
+
+

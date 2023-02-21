293baffb3b68aa3b7e703dcd60d50d6f3e30f490
nesterovski <mydeepsix@ya.ru>
2023-02-22 01:26:21 +0300

    faq added

---
+++ Speaking Club Project/app.js
@@ -0,0 +1,15 @@
+const items = document.querySelectorAll(".accordion button");
+
+items.forEach((item) => item.addEventListener("click", toggleAccordion));
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute("aria-expanded");
+
+  for (let item of items) {
+    item.setAttribute("aria-expanded", false);
+  }
+
+  if (itemToggle === "false") {
+    this.setAttribute("aria-expanded", true);
+  }
+}
